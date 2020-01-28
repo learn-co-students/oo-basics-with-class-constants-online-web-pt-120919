d353414a31f023143c363bd83b127925cc5be292
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-28 00:26:29 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,9 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count # remove the attr_accessor for genre
+  attr_reader :title, :genre # add an attr_reader for genre
+ 
+  
+  GENRES = ["Thriller", "Science Fiction", "Romance"]
 
   def initialize(title)
     @title = title
@@ -9,5 +12,13 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+  
+  # create the writer for genre and add the logic for the class constant
+  
+  #passed without the writer for genre and kept it on attr_accessor
+    def genre=(genre)
+        @genre = genre
+        GENRES << genre
+    end
 
 end
